edc201f22388912f1869fb2943bbae33e82bf91a
Mariusz94 <lyszczarz.mariusz@gmail.com>
2023-09-18 16:32:47 +0200

    add gen message

---
+++ app/grpc_file/bs_authentication_msg/authentication_pb2.pyi
@@ -0,0 +1,30 @@
+from default_msg import default_pb2 as _default_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class LoginData(_message.Message):
+    __slots__ = ["login", "password"]
+    LOGIN_FIELD_NUMBER: _ClassVar[int]
+    PASSWORD_FIELD_NUMBER: _ClassVar[int]
+    login: str
+    password: str
+    def __init__(self, login: _Optional[str] = ..., password: _Optional[str] = ...) -> None: ...
+
+class UserInfo(_message.Message):
+    __slots__ = ["address", "first_name", "id", "last_name", "login", "phone_number"]
+    ADDRESS_FIELD_NUMBER: _ClassVar[int]
+    FIRST_NAME_FIELD_NUMBER: _ClassVar[int]
+    ID_FIELD_NUMBER: _ClassVar[int]
+    LAST_NAME_FIELD_NUMBER: _ClassVar[int]
+    LOGIN_FIELD_NUMBER: _ClassVar[int]
+    PHONE_NUMBER_FIELD_NUMBER: _ClassVar[int]
+    address: str
+    first_name: str
+    id: str
+    last_name: str
+    login: str
+    phone_number: str
+    def __init__(self, id: _Optional[str] = ..., first_name: _Optional[str] = ..., last_name: _Optional[str] = ..., phone_number: _Optional[str] = ..., address: _Optional[str] = ..., login: _Optional[str] = ...) -> None: ...
